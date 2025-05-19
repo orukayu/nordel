4c52316e2372647b731971f78f0282af4da5bdc9
oktay <oktaybayram@hotmail.com>
2025-05-20 01:00:03 +0300

    ana sayfa tablo duzenı degıstırıldı

--- bunem/forms.py
+++ bunem/forms.py
@@ -7,9 +7,9 @@ class DegerlerForm(forms.ModelForm):
         fields = ['Airflow', 'GKT_Sicakligi', 'GB_Nemi', 'CKT_Sicakligi', 'CB_Nemi']
         labels = {'Airflow' : 'Hava Debisi', 'GKT_Sicakligi' : 'GKT Sıcaklığı', 'GB_Nemi' : 'GB Nemi', 'CKT_Sicakligi' : 'ÇKT Sıcaklığı', 'CB_Nemi' : 'ÇB Nemi'}
         widgets = {
-            'Airflow': forms.TextInput(attrs={'placeholder': '3.62'}),
-            'GKT_Sicakligi': forms.TextInput(attrs={'placeholder': '23'}),
-            'GB_Nemi': forms.TextInput(attrs={'placeholder': '80'}),
-            'CKT_Sicakligi': forms.TextInput(attrs={'placeholder': '14.8'}),
-            'CB_Nemi': forms.TextInput(attrs={'placeholder': '95'}),
+            'Airflow': forms.TextInput(attrs={'class': 'form-control', 'placeholder': '3.62'}),
+            'GKT_Sicakligi': forms.TextInput(attrs={'class': 'form-control', 'placeholder': '23'}),
+            'GB_Nemi': forms.TextInput(attrs={'class': 'form-control', 'placeholder': '80'}),
+            'CKT_Sicakligi': forms.TextInput(attrs={'class': 'form-control', 'placeholder': '14.8'}),
+            'CB_Nemi': forms.TextInput(attrs={'class': 'form-control', 'placeholder': '95'}),
         }
